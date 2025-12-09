0da65d11978ebd5bffb095590ef7e73e51fed257
gshildkrot <gshildkrot@gmail.com>
2025-12-09 09:56:56 -0500

    Update black_scholes_simulation.py

--- black_scholes_simulation.py
+++ black_scholes_simulation.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 S0 = 100        # Current Price ($)
 K = 100         # Strike Price ($)
 T = 1.0         # Time to maturity (Years)
-r = 0.3611      # Risk free rate (Treasury Rate for T)
+r = 0.03611     # Risk free rate (Treasury Rate for T)
 sigma = 0.1000  # Volatility (For actual stocks, it can be found on Yahoo Finance (or others) under Implied Volatility)
 steps = 252     # Trading days in a year
 dt = T / steps  # Time step size
@@ -49,6 +49,7 @@ plt.show()
 num_sims = 10000    # Number of times the simulation is run
 payoffs = []
 
+# Here unlike the first part, we don't run every single day, we just skip to the last day by exchangability.
 for i in range(num_sims):
     Z = np.random.normal(0, 1)
     drift_total = (r - 0.5 * sigma**2) * T
@@ -58,13 +59,18 @@ for i in range(num_sims):
     payoff = max(final_price - K, 0)
     payoffs.append(payoff)
 
+# The price of the option needs to be discounted by the money that would have been earned risk-free
 average_payoff = np.mean(payoffs)
 option_price = average_payoff * np.exp(-r * T)
+
+print(f"Parameters: S0=${S0}, K=${K}, Vol={sigma*100}%, T={T} Yr")
 
 print(f"Estimated Option Price: ${option_price:.2f}")
 
 # CHECK ANSWER
 from scipy.stats import norm
+
+# This is just the Black-Scholes Equation we are checking against
 
 d1 = (np.log(S0/K) + (r + 0.5*sigma**2)*T) / (sigma*np.sqrt(T))
 d2 = d1 - sigma*np.sqrt(T)
